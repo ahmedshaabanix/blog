dba21b1a5a22d602e4ca3a856e109a404f8a32e0
Ahmed Shaaban <ahmedshaabanix@gmail.com>
2021-06-25 17:03:27 +0200

    Update settings.py

--- src/project/settings.py
+++ src/project/settings.py
@@ -134,7 +134,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # remove before upload in github
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'gabr40641@gmail.com'
-EMAIL_HOST_PASSWORD = '15152525@@'
+EMAIL_HOST_USER = 'mail@gmail.com'
+EMAIL_HOST_PASSWORD = '***********'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
